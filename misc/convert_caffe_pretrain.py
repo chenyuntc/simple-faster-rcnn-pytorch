5bfbd2c24adfb32a03e600825465a2f4fd20cbf0
chenyuntc <chenyuntc@163.com>
2017-12-22 21:31:09 +0800

    add scripts for converting caffe pretrain model

---
+++ misc/convert_caffe_pretrain.py
@@ -0,0 +1,20 @@
+# code from ruotian luo
+# https://github.com/ruotianluo/pytorch-faster-rcnn
+import torch
+from torch.utils.model_zoo import load_url
+from torchvision import models
+
+sd = load_url("https://s3-us-west-2.amazonaws.com/jcjohns-models/vgg16-00b39a1b.pth")
+sd['classifier.0.weight'] = sd['classifier.1.weight']
+sd['classifier.0.bias'] = sd['classifier.1.bias']
+del sd['classifier.1.weight']
+del sd['classifier.1.bias']
+
+sd['classifier.3.weight'] = sd['classifier.4.weight']
+sd['classifier.3.bias'] = sd['classifier.4.bias']
+del sd['classifier.4.weight']
+del sd['classifier.4.bias']
+
+
+# speicify the path to save
+torch.save(sd, "vgg16_caffe.pth")
