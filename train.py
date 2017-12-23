4131f2bc6576436eb064c321722ebdc9bbf14ff4
chenyuntc <chenyuntc@163.com>
2017-12-24 00:28:50 +0800

    fix ulimit

--- train.py
+++ train.py
@@ -15,6 +15,12 @@ from util import array_tool as at
 from util.vis_tool import visdom_bbox
 from util.eval_tool import eval_detection_voc
 
+# fix for ulimit
+# https://github.com/pytorch/pytorch/issues/973#issuecomment-346405667
+import resource
+rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
+resource.setrlimit(resource.RLIMIT_NOFILE, (20480, rlimit[1]))
+
 matplotlib.use('agg')
 
 def eval(dataloader, faster_rcnn, test_num=10000):
