367db367834efd8a2bc58ee0023b2b628a0e474d
Yun Chen <i@knew.be>
2020-07-18 10:07:01 -0400

    fix visdom server path

--- utils/vis_tool.py
+++ utils/vis_tool.py
@@ -175,7 +175,7 @@ class Visualizer(object):
     """
 
     def __init__(self, env='default', **kwargs):
-        self.vis = visdom.Visdom('ag.knew.be',env=env, use_incoming_socket=False, **kwargs)
+        self.vis = visdom.Visdom('localhost',env=env, use_incoming_socket=False, **kwargs)
         self._vis_kw = kwargs
 
         # e.g.('loss',23) the 23th value of loss
